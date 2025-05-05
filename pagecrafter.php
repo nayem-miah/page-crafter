4aca4981f7105d370c147caf8c8506e20e28c01e
Nayem Miah <naeemislam13790@gmail.com>
2025-05-05 15:43:45 +0600

    'commited'

--- pagecrafter.php
+++ pagecrafter.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name:       PageCrafter
  * Plugin URI:  https://nayemmiah.com/
--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
 
-$someVar= "Hello World";
+$someVar = 'Hello World';
 
-function testFunction( $arg1 ,$arg2){
-echo "<div>".$arg1. " " .$arg2."</div>";
+function testFunction( $arg1, $arg2 ) {
+	echo '<div>' . $arg1 . ' ' . $arg2 . '</div>';
 }
 
-testFunction( $someVar , "<script>alert('xss');</script>" );
+testFunction( $someVar, "<script>alert('xss');</script>" );
 
-if ( true ){
-echo 'This should be indented.';
+if ( true ) {
+	echo 'This should be indented.';
 }
